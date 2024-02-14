cfca1b9859a6f47e7cab292c3092fb78b0d0c7df
Hamit1903 <williamakbal@gmail.com>
2024-02-13 18:54:29 -0800

    second commit

--- src/main/java/org/syntax/Main.java
+++ src/main/java/org/syntax/Main.java
@@ -3,5 +3,7 @@ package org.syntax;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("dadsad");
+        System.out.println("2343242");
     }
 }
